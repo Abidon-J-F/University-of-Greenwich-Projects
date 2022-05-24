788e81d587e90a740ef50b9072ff4e2d5c0578f7
Abidon-J-F <106085589+Abidon-J-F@users.noreply.github.com>
2022-05-24 21:38:42 +0100

    Update CourseworkOverview.java

--- Y1/Notebook/src/coursework/CourseworkOverview.java
+++ Y1/Notebook/src/coursework/CourseworkOverview.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coursework;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author Abidon
+ * @author Abidon Jude Fernandes
  */
 public class CourseworkOverview extends Coursework {
 
